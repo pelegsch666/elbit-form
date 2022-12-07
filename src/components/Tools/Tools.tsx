6d93b0764144a4611b1c7e47f86b8e099e9abbd7
pelegsch666 <peleg.mazuz@gmail.com>
2022-12-07 16:22:17 +0200

    last update

--- src/components/Tools/Tools.tsx
+++ src/components/Tools/Tools.tsx
@@ -1,71 +1,69 @@
-import { useRecoilState, useSetRecoilState } from 'recoil';
+import { useRecoilState, useSetRecoilState } from "recoil";
 
-import { Button } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Button } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 import {
-	currInputsDataState,
-	currItemIndexState,
-	isEditModeOnState,
-	tableDataState
-} from 'store';
+  currInputsDataState,
+  currItemIndexState,
+  isEditModeOnState,
+  tableDataState,
+} from "store";
 
 const StyledButton = styled(Button)(({ theme }) => ({
-	margin: 3,
-	padding: 0,
-	
+  margin: 3,
+  padding: 0,
 }));
 
 type ToolsProps = {
-	itemIndex: number;
+  itemIndex: number;
 };
 
 export default function Tools({ itemIndex }: ToolsProps) {
-	const [tableData, setTableData] = useRecoilState(tableDataState);
-	const [isEditModeOn, setIsEditModeOn] = useRecoilState(isEditModeOnState);
-	const setCurrItemIndex = useSetRecoilState(currItemIndexState);
-	const [currInputsData, setCurrInputsData] =
-		useRecoilState(currInputsDataState);
+  const [tableData, setTableData] = useRecoilState(tableDataState);
+  const [isEditModeOn, setIsEditModeOn] = useRecoilState(isEditModeOnState);
+  const setCurrItemIndex = useSetRecoilState(currItemIndexState);
+  const setCurrInputsData = useSetRecoilState(currInputsDataState);
 
-	const trashEmoji = '🗑️';
-	const editEmoji = '✏️';
+  const trashEmoji = "🗑️";
+  const editEmoji = "✏️";
 
-	function handleDelete() {
-		const newData = tableData.filter((item, index) => index !== itemIndex);
-		setTableData(newData);
-		const stringifiedNewData = JSON.stringify(newData);
-		localStorage.setItem('data', stringifiedNewData);
-		setCurrItemIndex(itemIndex);
-	}
+  function handleDelete() {
+    const newData = tableData.filter((item, index) => index !== itemIndex);
+    setTableData(newData);
+    const stringifiedNewData = JSON.stringify(newData);
+    localStorage.setItem("data", stringifiedNewData);
+    setCurrItemIndex(itemIndex);
+  }
 
-	function handleEdit() {
-		setIsEditModeOn(!isEditModeOn);
-		setCurrItemIndex(itemIndex);
-		const currItem = tableData[itemIndex];
-		if (!isEditModeOn) {
-			setCurrInputsData(currItem);
-		} else {
-			setCurrInputsData({
-				date: '',
-				boardPn: '',
-				boardSn: '',
-				techName: '',
-				boardDataBaseName: '',
-				causeFailure: '',
-				solution: '',
-				dataUrl: '',
-			});
-		}
-	}
+  function handleEdit() {
+    setIsEditModeOn(!isEditModeOn);
+    setCurrItemIndex(itemIndex);
+    const currItem = tableData[itemIndex];
+    if (!isEditModeOn) {
+      setCurrInputsData(currItem);
+    } else {
+      setCurrInputsData({
+        date: "",
+        boardPn: "",
+        boardSn: "",
+        techName: "",
+        boardDataBaseName: "",
+        causeFailure: "",
+        solution: "",
+        dataUrl: "",
+      });
+    }
+  }
 
-	return (
-		<>
-			<StyledButton variant="outlined" color="secondary" onClick={handleDelete}>
-				{trashEmoji}
-			</StyledButton>
-			<StyledButton variant="outlined" color="secondary" onClick={handleEdit}>
-				{editEmoji}
-			</StyledButton>
-		</>
-	);
+  return (
+    <>
+      <StyledButton variant="outlined" color="secondary" onClick={handleDelete}>
+        {trashEmoji}
+      </StyledButton>
+      <StyledButton variant="outlined" color="secondary" onClick={handleEdit}>
+        {editEmoji}
+      </StyledButton>
+    </>
+  );
 }
